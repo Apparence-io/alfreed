626bb3332db99a7fb91c559734d4ac0ab46a8652
Gautier <gautier@apparence.io>
2021-05-12 14:26:19 +0200

    changelog + licence

--- lib/alfreed.dart
+++ lib/alfreed.dart
@@ -6,5 +6,4 @@
 export './src/presenter.dart';
 export './src/context_wrapper.dart';
 export './src/page_builder.dart';
-export './src/utils.dart';
 export './src/models/anim.dart';
--- test/page_builder/multi_anim/builder_test.dart
+++ test/page_builder/multi_anim/builder_test.dart
@@ -1,4 +1,4 @@
-import 'package:alfreed/src/utils.dart';
+import 'file:///Users/mcfly/workspace/flutter-plugins/alfreed/test/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
