3fe98440645329b23b82346aae085a42bbcef488
Kiirii <k.nikolnikov@apparence.io>
2021-11-16 15:27:37 +0100

    Added Alfreed StatefulWidget implementation

--- lib/src/page.dart
+++ lib/src/page.dart
@@ -19,3 +19,29 @@ abstract class AlfreedPage<P extends Presenter, M, I extends AlfreedView>
 
   AlfreedPageBuilder<P, M, I> get alfreedPageBuilder;
 }
+
+abstract class AlfreedFulPage<P extends Presenter, M, I extends AlfreedView>
+    extends StatefulWidget {
+  final Object? args;
+
+  AlfreedFulPage({
+    Key? key,
+    this.args,
+  });
+
+  AlfreedPageBuilder<P, M, I> get alfreedPageBuilder;
+
+  @override
+  _AlfreedFulPageState createState() =>
+      _AlfreedFulPageState(builder: alfreedPageBuilder..args = args);
+}
+
+class _AlfreedFulPageState<P extends Presenter, M, I extends AlfreedView>
+    extends State<AlfreedFulPage> {
+  final AlfreedPageBuilder<P, M, I> builder;
+
+  _AlfreedFulPageState({required this.builder});
+
+  @override
+  Widget build(BuildContext context) => builder.build(context);
+}
